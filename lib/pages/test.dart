53d862ef1c6fc831f22e7ce3808c24fa33ff6da2
Muhammad Afzaal <118747946+afzaaljutt2981@users.noreply.github.com>
2024-11-05 21:12:54 +0500

    Update test.dart

--- lib/pages/test.dart
+++ lib/pages/test.dart
@@ -65,7 +65,7 @@
 //       setState(() {
 //         addMessage(Message(text: DialogText(text: [text])), true);
 //       });
-
+ 
 //       DetectIntentResponse response = await dialogFlowtter.detectIntent(
 //           queryInput: QueryInput(text: TextInput(text: text)));
 //       if (response.message == null) return;
